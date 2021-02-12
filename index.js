4ed97588db5832b24de5315ad50ccbd250fca9eb
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-12 04:29:04 +0000

    Almost ocrrect answer, minor bugs

---
+++ index.js
@@ -0,0 +1,17 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
+
+const printPassTimes = (passes) => {
+  for (const pass of passes) {
+    const date = new Date(0);
+    date.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${date} for ${duration} seconds`);
+  }
+};
---
+++ iss.js
@@ -0,0 +1,75 @@
+const request = require('request');
+
+
+const fetchMyIP = (ipCallback) => {
+  const urlIP = 'https://api64.ipify.org/?format=json';
+  request(urlIP, (error, response, body) => {
+    if (error) {
+      return ipCallback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return ipCallback(Error(msg), null);
+    }
+    const ip = JSON.parse(body).ip;
+    ipCallback(null, ip);
+
+  });
+};
+
+const fetchCoordsByIP = (ip, geoCallback) => {
+  const urlGEO = 'https://freegeoip.app/json/' + ip;
+  request(urlGEO, (error, response, body) => {
+    if (error) {
+      return geoCallback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      return geoCallback(Error(msg), null);
+    }
+    const { latitude, longitude} = JSON.parse(body);
+    geoCallback(null, { latitude, longitude});
+  });
+};
+
+
+const fetchISSFlyOverTimes = (coords, flyCallback) => {
+  const satUrl = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(satUrl, (error, response, body) => {
+    if (error) {
+      return flyCallback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching fly over times. Response: ${body}`;
+      return flyCallback(Error(msg), null);
+    }
+    const flyOver = JSON.parse(body).response;
+    flyCallback(flyOver);
+  });
+};
+
+
+const nextISSTimesForMyLocation = (chainCallback) => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return chainCallback('1:',error, null);
+    }
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        return chainCallback("2:",error, null);
+      }
+      
+      fetchISSFlyOverTimes(coordinates, (error, pass) => {
+        // if (error) {
+        //   console.log(error);
+        //   return chainCallback("3:", error, null);
+        // }
+        chainCallback(null, error);
+      });
+    });
+  });
+};
+
+
+module.exports = {nextISSTimesForMyLocation};
+
